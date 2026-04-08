66d5caef3d694752a3ccf55890ac15dd8521d51c
Murilo Kloppel <murilo.kloppel@outlook.com>
2026-04-07 22:03:47 -0300

    new image

--- App.tsx
+++ App.tsx
@@ -1,13 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View, Image } from 'react-native';
+import imgScorp from './assets/scorp.webp'
 
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image 
+      //source={require('.asset/scorp.webp')}
+      source={imgScorp}
+      />
+
+      <View
+      onTouchStart={(event) =>  {Alert.alert("Clique Start" , "Clique Começou")}}
+      onTouchMove={(event) =>  {Alert.alert("Clique End" , "Clique Terminou")}}
+      > </View>
       
       <Text style={styles.text}>Flexbox</Text>
       <Text style={styles.text}> ➡️➡️ </Text>
+
+      <Text
+      onPress={() => console.log("texto pressionado")}
+      onLongPress={() => console.log("long sendo utilizado")} 
+      
+          >     Frase para demonstração de eventos!!!</Text>
+          <View style={{flexDirection: "row", flexWrap: "wrap", justifyContent: "center"}}>
+                <Text style={styles.text}>Hello</Text>
+                <Text style={styles.text}>World</Text>
+          </View>
      
       <StatusBar style="auto" />
     </View>
@@ -17,17 +37,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
+    //flexDirection: "column",
     backgroundColor: '#63edd6',
     alignItems: 'center',
-    flexWrap: "wrap",
+    //flexWrap: "wrap",
     justifyContent: 'center',
     borderColor: "#f0f772",
     borderWidth: 12,
-    borderRadius: 50,
+    borderRadius: 70,
   },
   text: {
     fontSize: 30,
+    alignContent: "center",
     fontWeight: "bold",
     backgroundColor: "#fbf989",
     padding: 15,
