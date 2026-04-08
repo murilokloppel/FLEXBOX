7bbd575d94898cd961363b642d0360a2dce67644
Murilo Kloppel <murilo.kloppel@outlook.com>
2026-04-07 22:13:31 -0300

    scroll-view

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { Alert, StyleSheet, Text, View, Image } from 'react-native';
+import { Alert, StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 import imgScorp from './assets/scorp.webp'
+
 
 
 export default function App() {
   return (
+    <ScrollView>
     <View style={styles.container}>
+      
       <Image 
       //source={require('.asset/scorp.webp')}
       source={imgScorp}
@@ -28,9 +31,89 @@ export default function App() {
                 <Text style={styles.text}>Hello</Text>
                 <Text style={styles.text}>World</Text>
           </View>
+
+
+
+
+
+
+
+
+
+          <Image 
+      //source={require('.asset/scorp.webp')}
+      source={imgScorp}
+      />
+
+      <View
+      onTouchStart={(event) =>  {Alert.alert("Clique Start" , "Clique Começou")}}
+      onTouchMove={(event) =>  {Alert.alert("Clique End" , "Clique Terminou")}}
+      > </View>
+      
+      <Text style={styles.text}>Flexbox</Text>
+      <Text style={styles.text}> ➡️➡️ </Text>
+
+      <Text
+      onPress={() => console.log("texto pressionado")}
+      onLongPress={() => console.log("long sendo utilizado")} 
+      
+          >     Frase para demonstração de eventos!!!</Text>
+          <View style={{flexDirection: "row", flexWrap: "wrap", justifyContent: "center"}}>
+                <Text style={styles.text}>Hello</Text>
+                <Text style={styles.text}>World</Text>
+          </View>
      
+
+
+
+
+
+
+
+
+
+
+
+     
+
+          <Image 
+      //source={require('.asset/scorp.webp')}
+      source={imgScorp}
+      />
+
+      <View
+      onTouchStart={(event) =>  {Alert.alert("Clique Start" , "Clique Começou")}}
+      onTouchMove={(event) =>  {Alert.alert("Clique End" , "Clique Terminou")}}
+      > </View>
+      
+      <Text style={styles.text}>Flexbox</Text>
+      <Text style={styles.text}> ➡️➡️ </Text>
+
+      <Text
+      onPress={() => console.log("texto pressionado")}
+      onLongPress={() => console.log("long sendo utilizado")} 
+      
+          >     Frase para demonstração de eventos!!!</Text>
+          <View style={{flexDirection: "row", flexWrap: "wrap", justifyContent: "center"}}>
+                <Text style={styles.text}>Hello</Text>
+                <Text style={styles.text}>World</Text>
+          </View>
+
+
+
+
+
+
+
+
+
+
+
+
+
       <StatusBar style="auto" />
     </View>
+ </ScrollView>
   );
 }
 
